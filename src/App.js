4897c88a11350c0d2303a727aa1e4bef87e4e1f8
sanjay <sanjay@acaard.com>
2021-03-19 18:36:50 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import ReactFullpage from '@fullpage/react-fullpage';
+
+function App() {
+  return (
+    <div className="App">
+  
+
+
+
+    </div>
+  );
+}
+
+export default App;
